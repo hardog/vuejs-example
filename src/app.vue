d60aea4389be6be7874fb2067dcc4639708b8b94
逸莱 <yilai.zp@alibaba-inc.com>
2018-04-02 18:19:45 +0800

    save draft

--- src/app.vue
+++ src/app.vue
@@ -1,19 +1,29 @@
 <style scoped>
 div{
-  font-size: 20px;
-  color: #fff000;
+  font-size: 40px;
+  color: #000000;
 }
 </style>
 
 <template>
-  <div>{{msg}}</div>
+  <div>
+    <h4>hardog, 27 years old</h4>
+    <div> {{msg}}</div>
+    <img :src="imgUrl"/>
+    <img :src="largeUrl"/>
+  </div>
 </template>
 
 <script>
+import shareImg from '@assets/share.png';
+import largeImg from '@assets/large.png';
+
 export default{
   data(){
     return {
-      msg: 'hello world'
+      msg: 'hello world',
+      imgUrl: shareImg,
+      largeUrl: largeImg
     };
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,26 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const devServer = require('./dev.server');
+
+const assetsPath = path.resolve(__dirname, './assets');
+const srcPath = path.resolve(__dirname, './src');
 
 module.exports = {
+  devServer,
   mode: 'production',//development,production
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'index.js',
-    publicPath: './dist/'
+    filename: 'index.js'
   },
-  plugins: [
-    new HtmlWebpackPlugin()
-  ],
+  resolve: {
+    extensions: ['.js', '.vue'],
+    alias: {
+      '@src': srcPath,
+      '@assets': assetsPath
+    },
+  },
   module: {
     rules: [{
         test: /\.vue$/,
@@ -26,6 +34,16 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/
+      },{
+        test: /\.(png|gif|jpe?g|ttf)$/,
+        use: [{
+            loader: 'url-loader',
+            options: {
+              limit: 1024 * 4,
+              name: './[name].[hash:7].[ext]',
+            }
+          }
+        ]
       }
     ]
   },

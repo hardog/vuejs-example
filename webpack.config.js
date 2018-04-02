876a5dd42f0dbb76483812840d2a93d0db76d4c0
逸莱 <yilai.zp@alibaba-inc.com>
2018-04-02 15:32:44 +0800

    full

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,42 @@
 'use strict';
 
-let path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 
 module.exports = {
-  entry: './src/app.js',
+  mode: 'production',//development,production
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/assets/',
-    filename: 'app.js'
+    filename: 'index.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ],
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
-          loaders: {}
+          extractCSS: true
         }
-      },
-      {
+      },{
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/
       }
     ]
   },
-  resolve: {
-    alias: {
-    }
-  }
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'vuejs-example',
+      template: './src/index.html',
+      inlineSource: '.(js|css)$',
+      inject: true
+    }),
+    new HtmlWebpackInlineSourcePlugin(),
+    new ExtractTextPlugin('index.css')
+  ]
 };
